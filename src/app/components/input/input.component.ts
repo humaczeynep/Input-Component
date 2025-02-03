feb6de279a3b7ba6b0a6451de4d8bb09ee89a09b
zeynephumac <zeynephumac1229@gmail.com>
2025-02-04 00:56:32 +0300

    visible change

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -96,36 +96,41 @@ export class InputComponent {
   @Input() placeholder?: string;
   @Input() type: 'text' | 'number' | 'email' | 'date' | 'password' = 'text';
   @Input() required: boolean = false;
-  @Input() model: string | number | null = '';
+  @Input() inputValue: string | number | null = '';
 
-  @Output() modelChange = new EventEmitter<string | number | null>();
+  @Output() inputValueChange = new EventEmitter<string | number | null>();
   @Output() data = new EventEmitter<string | number | null>();
 
+  showPassword: boolean = false;
 
   touched: boolean = false;
 
   inputChange(value: string | number | null) {
-    this.modelChange.emit(value);
+    this.inputValueChange.emit(value);
+  }
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
   }
 
   sendData() {
     this.touched = true;
-    this.data.emit(this.model);
+    this.data.emit(this.inputValue);
   }
 
   get errorMessage(): string {
     if (!this.touched) return '';
 
-    if (this.required && !this.model) {
+    if (this.required && !this.inputValue) {
         return 'Bu alan zorunludur.';
     }
-    if (this.type === 'email' && this.model && !/^\S+@\S+\.\S+$/.test(this.model.toString())) {
+    if (this.type === 'email' && this.inputValue && !/^\S+@\S+\.\S+$/.test(this.inputValue.toString())) {
       return 'Geçerli bir e-posta adresi giriniz.';
     }
-    if (this.type === 'number' && this.model && Number(this.model) < 0) {
+    if (this.type === 'number' && this.inputValue && Number(this.inputValue) < 0) {
       return 'Sayı sıfırdan küçük olamaz.';
     }
-    if (this.type === 'password' && this.model && !/(?=.*[A-Z])(?=.*[!%&?])/.test(this.model.toString())) {
+    if (this.type === 'password' && this.inputValue && !/(?=.*[A-Z])(?=.*[!%&?])/.test(this.inputValue.toString())) {
       return 'Parola en az bir büyük harf ve bir özel karakter (!%&?) içermelidir.';
     }
     return '';
